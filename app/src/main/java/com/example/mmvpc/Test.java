c076db35cba3ba0527f821efe2b30b1f67693661
Administrator <huangshenshen666@163.com>
2017-09-13 17:02:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mmvpc/Test.java
@@ -0,0 +1,15 @@
+package com.example.mmvpc;
+
+
+import android.util.Log;
+
+
+/**
+ * Created by Administrator on 2017/9/13 0013.
+ */
+
+public class Test {
+    public void Login(){
+        Log.i("aaa","已经试过了login");
+    }
+}
